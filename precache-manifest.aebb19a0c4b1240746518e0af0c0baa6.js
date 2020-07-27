7861933c5b66d5e440662636b41e93019a23491b
Doris <d1882002@gmail.com>
2020-07-27 19:38:26 +0800

    Updates

---
+++ precache-manifest.aebb19a0c4b1240746518e0af0c0baa6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaf10d38d38694dc306d71d2d4771011",
+    "url": "/beezontop/react-shopping/index.html"
+  },
+  {
+    "revision": "0f63032fa8785f5dad36",
+    "url": "/beezontop/react-shopping/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "6820b9c93dde479dbfa9",
+    "url": "/beezontop/react-shopping/static/css/main.c3bc0c09.chunk.css"
+  },
+  {
+    "revision": "0f63032fa8785f5dad36",
+    "url": "/beezontop/react-shopping/static/js/2.83bc9c7a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/beezontop/react-shopping/static/js/2.83bc9c7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6820b9c93dde479dbfa9",
+    "url": "/beezontop/react-shopping/static/js/main.89d9a7c6.chunk.js"
+  },
+  {
+    "revision": "2be3c338a7cbd4fafdf3",
+    "url": "/beezontop/react-shopping/static/js/runtime-main.305e49d2.js"
+  }
+]);
