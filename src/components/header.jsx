ad5427f5763009cc52c1b9a43b3d6130f6310223
Doris <d1882002@gmail.com>
2020-08-11 14:20:05 +0800

    color changed

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,13 +1,12 @@
-import React from 'react';
-import '../css/header.css';
+import React from "react";
+import "../css/header.css";
 
 const Header = () => {
-    
-    return ( <header>
-        <h1 className="full-width">
-           <span className="color-red">DOMA</span><span className="color-black">ZON</span>
-        </h1>
-    </header> );
-}
- 
+  return (
+    <header>
+      <h1 className="full-width">DOMAZON</h1>
+    </header>
+  );
+};
+
 export default Header;
